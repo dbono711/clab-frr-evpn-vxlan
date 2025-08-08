c7ee599d9bf1ed835795f8d5a8e4073855fa2814
Darren Bono <dbono215@gmail.com>
2025-08-08 01:41:10 +0000

    refactor: restructure repository to support multiple EVPN/VXLAN lab topologies

--- evpn_ebgp_over_ipv4_ebgp_bridged_overlay/gateway/gateway.sh
+++ evpn_ebgp_over_ipv4_ebgp_bridged_overlay/gateway/gateway.sh
@@ -8,7 +8,6 @@ chmod 775 /etc/frr/logs
 ip link add name eth1.10 link eth1 type vlan id 10
 # Bring up the VLAN interface
 ip link set dev eth1.10 up
-
 # Create a VLAN interface for Tenant 20
 ip link add name eth1.20 link eth1 type vlan id 20
 # Bring up the VLAN interface
